e99ad03d54f14d38c25093ea2a9b3ee5bb5359ef
ardaerdogdu <ardaerdogdu@hotmail.com>
2023-08-05 02:08:00 +0300

    QR-Code-Generator
    An application developed with the c# qrcoder package, where you can enter the desired url and save a png image in qr format as a result.

---
+++ qrCode/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace qrCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GenerateQRCode = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GenerateQRCode
+            // 
+            this.GenerateQRCode.BackColor = System.Drawing.Color.Gray;
+            this.GenerateQRCode.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GenerateQRCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.GenerateQRCode.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.GenerateQRCode.Location = new System.Drawing.Point(167, 219);
+            this.GenerateQRCode.Name = "GenerateQRCode";
+            this.GenerateQRCode.Size = new System.Drawing.Size(201, 109);
+            this.GenerateQRCode.TabIndex = 0;
+            this.GenerateQRCode.Text = "QR OLUŞTUR";
+            this.GenerateQRCode.UseVisualStyleBackColor = false;
+            this.GenerateQRCode.Click += new System.EventHandler(this.GenerateQRCode_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(12, 114);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(502, 50);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(73, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(389, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "QR KOD OLUŞTURUCU";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(526, 408);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.GenerateQRCode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QR KOD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerateQRCode;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ qrCode/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace qrCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateQRCode_Click(object sender, EventArgs e)
+        {
+
+            string url = textBox1.Text.Trim();
+
+            if (!string.IsNullOrEmpty(url))
+            {
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+
+                using (SaveFileDialog saveDialog = new SaveFileDialog())
+                {
+                    saveDialog.Filter = "PNG Files (*.png)|*.png";
+                    saveDialog.FilterIndex = 1;
+                    saveDialog.FileName = "QRCode.png";
+
+                    if (saveDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        Bitmap qrCodeImage = qrCode.GetGraphic(10); 
+                        qrCodeImage.Save(saveDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                        MessageBox.Show("QR kod kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir URL girin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        
+
+       
+        }
+    }
+}
